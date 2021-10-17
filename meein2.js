a4070411e2a7976fc552184dfe09a62575a69569
samialharbi96 <sami.sagier.js@tuwaiq.edu.sa>
2021-10-17 20:18:15 +0300

    tassk2

--- meein2.js
+++ meein2.js
@@ -1,31 +1,61 @@
-const addToArray = function (array, string) {
 
-    array.push(string)
-    return array
+  let addToArray = function (Array,string) {
+    Array.push(string)
+return Array;
 }
 
 
-const convertToString = function (array) {
-  // TODO: Your code here
-  return array.join("@")
 
-};
 
-const updateOrCreate = function (array, value, index) {
-    // TODO: Your code here
-if (index > array.length){
-    return "there is no element to updated in this index"
-}else{
-    array[index] =value
-    return array 
+const convertToString = function (array_1) {
+
+return array_1.join('   ')
 
 }
 
 
-};
+
+
+const updateOrCreate = function (array, index, value) {
+ if (index > array.length) {
+    array[index]= value
+
+     return "there is no element to updated in this index";
+
+ }else{
+    array[index]= value
+return array ;
+}}
+
 
 
 const onlyString = function (array) {
-    // TODO: Your code here
-  };
 
+for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] == "Number") {
+        result.slice(array[i]);
+return array;
+ }
+
+
+    }
+}  
+const users = [
+    ["Jane", "123456" ],
+    ["admin", "abc123" ],
+  ];
+  const login = function (username, password) {
+
+    // if (username == users[0][0]  &&  password == users [0][1]){
+    //     return 'Login Successful' ;
+    // }else if (username == users[1][0] && password == users [1][1]){
+    //     return "Login Successful ";
+    // }else{
+    //     return " Login Failed";
+
+    // }
+    for (let i = 0; i <=1; ++i) {
+        if (username == users[0][i]  &&  password == users [0][i]){
+                return 'Login Successful' ;
+
+  }return 'Login Failed';}}
